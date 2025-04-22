46771f9815b86768ea13feaed9ccd206fadc9c15
Yuanzhuoxin <1974269607@qq.com>
2025-04-22 16:36:36 +0000

    feat: 样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react'
 import Link from 'next/link'
-import { Button } from 'antd'
-import classnames from '@/utils/classnames'
 
-const projects = ['test1']
+const projects = ['1.three-playground']
 
 const Page: FC = () => {
-  return projects.map((name) => (
-    <Link href={name} key={name}>
-      <Button type='primary' className={classnames('bg-black')}>
-        {name}
-      </Button>
-    </Link>
-  ))
+  return (
+    <div className='mx-auto w-max flex flex-col p-4'>
+      {projects.map((name) => (
+        <Link className='text-xl' href={name} key={name}>
+          {name}
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default Page
