518fb69ca3b2d164e93627bba902ccc1ba120fc6
Yuanzhuoxin <1974269607@qq.com>
2025-04-26 16:34:47 +0000

    feat: 阴影

--- src/app/2.light&material/page.tsx
+++ src/app/2.light&material/page.tsx
@@ -1,18 +1,33 @@
 'use client'
 
 import { FC } from 'react'
+import { Helper, OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import {
+  DirectionalLightHelper,
+  DirectionalLightShadow,
+  PointLightHelper,
+} from 'three'
+import { Axes } from '@/components/Axes'
 
 const Page: FC = () => {
   return (
-    <Canvas>
-      <directionalLight></directionalLight>
-      <mesh>
+    <Canvas shadows>
+      <Axes />
+      <OrbitControls />
+      <ambientLight></ambientLight>
+      <directionalLight castShadow position={[1, 1, 1]}>
+        <Helper type={DirectionalLightHelper} args={[1, 'red']}></Helper>
+      </directionalLight>
+      <mesh castShadow>
         <boxGeometry></boxGeometry>
-        <meshPhysicalMaterial />
+        <meshPhysicalMaterial color={'red'} />
+      </mesh>
+      <mesh receiveShadow position={[0, -1, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial />
       </mesh>
     </Canvas>
   )
 }
-
 export default Page
